b316be9ce88e78a3ff9e40cb00727fcddd968053
UchiwaFuujinn <uchiwafuujinn@nifty.com>
2020-02-08 15:06:57 +0900

    Update IoTmidi.js

--- IoTmidi.js
+++ IoTmidi.js
@@ -1,22 +1,3 @@
-/*
-window.onload=function(){
-	runTest2();
-	var hoge = setInterval(function() {
-	    //wait webmidi start
-		if (inputs != null) {
-			setInputMenuID(document.input_device_select.ids);
-			setOutputMenuID(document.output_device_select.ids);
-			if(input_menu_id!=null){ setInputDeviceSelect();
-				input.onmidimessage = handleMIDIMessage3;
-			}
-			if(output_menu_id!=null) setOutputDeviceSelect();
-			clearInterval(hoge);
-
-		}
-	}, 200);
-}
-*/
-
 var mlognum=0;
 var ColumnNum=80;
 var stoplog=1;
